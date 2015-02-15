d90627246dbdd515adf3c9528aed858c5a20e083
Thomas Ballinger <thomasballinger@gmail.com>
2015-02-14 20:26:54 -0500

    initial state from http://underscores.me/

---
+++ inc/jetpack.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Jetpack Compatibility File
+ * See: http://jetpack.me/
+ *
+ * @package ontrei
+ */
+
+/**
+ * Add theme support for Infinite Scroll.
+ * See: http://jetpack.me/support/infinite-scroll/
+ */
+function ontrei_jetpack_setup() {
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'footer'    => 'page',
+	) );
+}
+add_action( 'after_setup_theme', 'ontrei_jetpack_setup' );
