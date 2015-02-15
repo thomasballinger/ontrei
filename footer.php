bc640e5d369657df90d188eccc516021691a3cf8
Thomas Ballinger <thomasballinger@gmail.com>
2015-02-14 20:51:01 -0500

    tabs to 2 spaces in php and js files

--- footer.php
+++ footer.php
@@ -8,15 +8,15 @@
  */
 ?>
 
-	</div><!-- #content -->
+  </div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'ontrei' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'ontrei' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'ontrei' ), 'ontrei', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+  <footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="site-info">
+      <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'ontrei' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'ontrei' ), 'WordPress' ); ?></a>
+      <span class="sep"> | </span>
+      <?php printf( __( 'Theme: %1$s by %2$s.', 'ontrei' ), 'ontrei', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+    </div><!-- .site-info -->
+  </footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) ) {
-	$content_width = 640; /* pixels */
+  $content_width = 640; /* pixels */
 }
 
 if ( ! function_exists( 'ontrei_setup' ) ) :
@@ -22,58 +22,58 @@ if ( ! function_exists( 'ontrei_setup' ) ) :
  */
 function ontrei_setup() {
 
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on ontrei, use a find and replace
-	 * to change 'ontrei' to the name of your theme in all the template files
-	 */
-	load_theme_textdomain( 'ontrei', get_template_directory() . '/languages' );
+  /*
+   * Make theme available for translation.
+   * Translations can be filed in the /languages/ directory.
+   * If you're building a theme based on ontrei, use a find and replace
+   * to change 'ontrei' to the name of your theme in all the template files
+   */
+  load_theme_textdomain( 'ontrei', get_template_directory() . '/languages' );
 
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+  // Add default posts and comments RSS feed links to head.
+  add_theme_support( 'automatic-feed-links' );
 
-	/*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
+  /*
+   * Let WordPress manage the document title.
+   * By adding theme support, we declare that this theme does not use a
+   * hard-coded <title> tag in the document head, and expect WordPress to
+   * provide it for us.
+   */
+  add_theme_support( 'title-tag' );
 
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
-	 */
-	//add_theme_support( 'post-thumbnails' );
+  /*
+   * Enable support for Post Thumbnails on posts and pages.
+   *
+   * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+   */
+  //add_theme_support( 'post-thumbnails' );
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'ontrei' ),
-	) );
+  // This theme uses wp_nav_menu() in one location.
+  register_nav_menus( array(
+    'primary' => __( 'Primary Menu', 'ontrei' ),
+  ) );
 
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
-	) );
+  /*
+   * Switch default core markup for search form, comment form, and comments
+   * to output valid HTML5.
+   */
+  add_theme_support( 'html5', array(
+    'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
+  ) );
 
-	/*
-	 * Enable support for Post Formats.
-	 * See http://codex.wordpress.org/Post_Formats
-	 */
-	add_theme_support( 'post-formats', array(
-		'aside', 'image', 'video', 'quote', 'link',
-	) );
+  /*
+   * Enable support for Post Formats.
+   * See http://codex.wordpress.org/Post_Formats
+   */
+  add_theme_support( 'post-formats', array(
+    'aside', 'image', 'video', 'quote', 'link',
+  ) );
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'ontrei_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
+  // Set up the WordPress core custom background feature.
+  add_theme_support( 'custom-background', apply_filters( 'ontrei_custom_background_args', array(
+    'default-color' => 'ffffff',
+    'default-image' => '',
+  ) ) );
 }
 endif; // ontrei_setup
 add_action( 'after_setup_theme', 'ontrei_setup' );
@@ -84,15 +84,15 @@ add_action( 'after_setup_theme', 'ontrei_setup' );
  * @link http://codex.wordpress.org/Function_Reference/register_sidebar
  */
 function ontrei_widgets_init() {
-	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'ontrei' ),
-		'id'            => 'sidebar-1',
-		'description'   => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
+  register_sidebar( array(
+    'name'          => __( 'Sidebar', 'ontrei' ),
+    'id'            => 'sidebar-1',
+    'description'   => '',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</aside>',
+    'before_title'  => '<h1 class="widget-title">',
+    'after_title'   => '</h1>',
+  ) );
 }
 add_action( 'widgets_init', 'ontrei_widgets_init' );
 
@@ -100,15 +100,15 @@ add_action( 'widgets_init', 'ontrei_widgets_init' );
  * Enqueue scripts and styles.
  */
 function ontrei_scripts() {
-	wp_enqueue_style( 'ontrei-style', get_stylesheet_uri() );
+  wp_enqueue_style( 'ontrei-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'ontrei-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+  wp_enqueue_script( 'ontrei-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
-	wp_enqueue_script( 'ontrei-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+  wp_enqueue_script( 'ontrei-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+  if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+    wp_enqueue_script( 'comment-reply' );
+  }
 }
 add_action( 'wp_enqueue_scripts', 'ontrei_scripts' );
 
